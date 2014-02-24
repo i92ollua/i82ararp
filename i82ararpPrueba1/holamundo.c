9dadf64c80bcc5fa1ee03e1bac5164e5ea0d2eaf
i92ollua <alex.luk91@gmail.com>
2014-02-24 16:46:30 +0100

    soy i92ollua
    Te he arreglado los fallos ;)

--- i82ararpPrueba1/holamundo.c
+++ i82ararpPrueba1/holamundo.c
@@ -1,5 +1,5 @@
 # include "./includes/miinclude.h"
 int main(){
 char c[100]= "Hola mundo";
-printf("%s\n", strcat(c,", soy i82ararp"));
+printf("%s\n", strcat(c,", soy i82ararp asfdasdasdas"));
 return 0; }
